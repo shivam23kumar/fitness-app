1f25350b5bbeb53f02a5ac118215ac2405838e16
shivam23kumar <shivamkumar.dev23@gmail.com>
2023-04-19 04:37:10 +0530

    Login.jsx

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./components/Home/Home";
 import Courses from "./components/Courses/Courses";
 import Header from "./components/Layout/Header/Header";
 import Footer from "./components/Layout/Footer/Footer";
+import Login from "./components/Auth/Login";
 function App() {
   return <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<Courses />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
